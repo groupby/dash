9e680078fe7e1881ed74d44066b5cd750403ab59
Ben Teichman <ben.l.teichman@gmail.com>
2016-09-20 14:01:19 -0400

    codacy

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const IO = require('koa-socket');
 const serve = require('koa-static');
 const jobFactory = require('./src/index');
 
+const PORT = 8080;
+
 const jobs = [];
 const app = new Koa();
 const io = new IO();
 const router = new Router({ prefix: '/api' });
 
-app.use(serve(`./static/`));
+app.use(serve('./static/'));
 
 router.get('/jobs', (ctx) => ctx.body = jobs.map((job) => Object.assign({}, job, { job: undefined })));
 router.post('/refresh', (ctx) => {
@@ -19,6 +21,6 @@ router.post('/refresh', (ctx) => {
 
 app.use(router.routes());
 io.attach(app);
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || PORT);
 
 jobFactory.generate({ io, jobs });
